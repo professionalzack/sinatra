8f6784fe96e8db69ec6d06cb9059787b3ec498a2
zack <zack.etc@gmail.com>
2017-12-20 16:12:54 -0800

    one small change

--- nancy.rb
+++ nancy.rb
@@ -6,26 +6,6 @@ module Nancy
       @routes = {}
     end
 
-
-    def call(env)
-        @request = Rack::Request.new(env)
-        verb = @request.request_method
-        requested_path = @request.path_info
-
-        handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
-
-        if handler
-            result = instance_eval(&handler)
-            if result.class == String
-                [200, {}, [result]]
-            else
-                result
-            end
-        else
-            [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
-        end
-    end
-
     attr_reader :routes
 
     def get(path, &handler)
@@ -46,6 +26,29 @@ module Nancy
 
     def delete(path, &handler)
         route("DELETE", path, &handler)
+    end
+
+    def head(path, &handler)
+      route("HEAD", path, &handler)
+    end
+
+    def call(env)
+        @request = Rack::Request.new(env)
+        verb = @request.request_method
+        requested_path = @request.path_info
+
+        handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+
+        if handler
+            result = instance_eval(&handler)
+            if result.class == String
+                [200, {}, [result]]
+            else
+                result
+            end
+        else
+            [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
+        end
     end
 
     attr_reader :request
